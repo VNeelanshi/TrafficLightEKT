b7894088ce6e28a3d53eee276e6b2d587415aa3b
Bijal Mehta <bijalm99@gmail.com>
2020-03-10 12:02:01 -0500

    bijal changes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     var ACTIVE_ATTR = "aria-active";
     var QUIZ_ATTR = "data-quiz-parent";
     var QUESTION_ATTR = "data-quiz-question-id";
+    var QUESTION_VAL = "data-quiz-question-value";
     var ANSWER_ATTR = "data-quiz-answer";
     var MULTIPLE_ID_ATTR = "data-quiz-multiple-id";
     var MULTIPLE_ATTR = "data-quiz-multiple";
@@ -12,10 +13,12 @@
     var FORK_BTN_ATTR = "data-quiz-fork-answer";
     var NEXT_ATTR = "data-quiz-next";
     var RESULTS_ATTR = "data-quiz-results";
+    var RESULTS_SUM = "data-quiz-sum";
     var RESULTS_DISPLAY_ATTR = "data-quiz-results-display";
     var RESTART_ATTR = "data-quiz-restart";
     var RETURN_ATTR = "data-quiz-return";
-    var answers = {};
+    var TOTAL = 53
+    var answers = [];
     
     function setActive(card) {
       card.setAttribute(ACTIVE_ATTR, "true");
@@ -28,7 +31,7 @@
     }
 
     function registerAnswer(question, answer) {
-      answers[question] = answer;
+      answers.push([question, answer])
     }
 
     function prevQuestion(current, prev) {
@@ -66,6 +69,30 @@
       for (var c = 0; c < checkboxes.length; c++) {
         checkboxes[c].checked = false;
       }
+      }
+    function riskCalculation() {
+        var positive = 0;
+        for (var a = 0; a < answers.length; a++) {
+            if (typeof answers[a][1] === 'object' && answers[a][1] !== null) {
+                for (var key in answers[a][1]) {
+                    if (answers[a][1].hasOwnProperty(key)) {
+                        if (answers[a][1][key] == true) {
+                            positive++;
+                        }
+
+                    }
+                }
+            }
+            else {
+                if (answers[a][1] == "Yes") {
+                    positive++;
+
+                };
+            };
+        }
+        console.log("end", positive)
+        var risk_percent = Math.round((positive / TOTAL) * 100);
+        return risk_percent;
     }
 
     function quizAction(e) {
@@ -90,34 +117,46 @@
         return;
       }
 
-      var currentQuestionId = parseInt(currentCard.getAttribute(QUESTION_ATTR), 10);
-      //var answerValue = answerBtn.getAttribute(ANSWER_ATTR)
-      var answerValue = answerBtn.innerHTML;
-      // if (answerBtn.getAttribute(ANSWER_ATTR) === "multiple") {
-      //   var answerObject = {};
-      //   var answerGroup = currentCard.querySelectorAll('[' + MULTIPLE_ATTR + '="' + answerBtn.getAttribute(MULTIPLE_ID_ATTR) + '"]');
-      //   for (var a = 0; a < answerGroup.length; a++) {
-      //     answerObject[answerGroup[a].name] = answerGroup[a].checked;
-      //   }
-      //   var answerValue = answerObject;
-      // }
-      // else {
-      // 	var answerValue = parseInt(answerBtn.getAttribute(ANSWER_ATTR), 10) === 1 ? true : false; // Yes == 1 == true / No == 0 == false
-      // }
+        
+        var currentQuestionId = currentCard.querySelector('[' + QUESTION_VAL + ']').innerHTML;
+        
+        var answerValue = answerBtn.innerHTML;
+        if (answerBtn.getAttribute(ANSWER_ATTR) === "multiple") {
+            var answerObject = {};
+            var answerGroup = currentCard.querySelectorAll('[' + MULTIPLE_ATTR + '="' + answerBtn.getAttribute(MULTIPLE_ID_ATTR) + '"]');
+            for (var a = 0; a < answerGroup.length; a++) {
+                answerObject[answerGroup[a].name] = answerGroup[a].checked;
+            }
+            var answerValue = answerObject;
+        }
+        else {
+            var answerValue = answerBtn.innerHTML;
+            
+        }
 
-      var nextQuestionId = answerBtn.getAttribute(NEXT_ATTR);
-      var nextCard =
-        currentCard.closest("[" + QUIZ_ATTR + "]")
-            .querySelector('[' + (
-              nextQuestionId === "results"
-              ? RESULTS_ATTR
-              : QUESTION_ATTR + '="' + parseInt(nextQuestionId, 10) + '"'
-            ) + ']');
-      registerAnswer(currentQuestionId, answerValue);
-      if (nextQuestionId === 'results') {
-        nextCard.querySelector('[' + RESULTS_DISPLAY_ATTR + ']').innerHTML = '<pre style="text-align:left;">' + JSON.stringify(answers, null, 2) + '</pre>';
-      }
-      nextQuestion(currentCard, nextCard);
+        var nextQuestionId = answerBtn.getAttribute(NEXT_ATTR);
+        var nextCard =
+            currentCard.closest("[" + QUIZ_ATTR + "]")
+                .querySelector('[' + (
+                    nextQuestionId === "results"
+                        ? RESULTS_ATTR
+                        : QUESTION_ATTR + '="' + parseInt(nextQuestionId, 10) + '"'
+                ) + ']');
+        registerAnswer(currentQuestionId, answerValue);
+        if (nextQuestionId === 'results') {
+            var risk_val = riskCalculation()
+            var level = "LOW"
+            if (risk_val <= 34) { level = "LOW"; };
+            if ((risk_val > 34) && (risk_val <= 68)) { level = "MEDIUM"; };
+            if ((risk_val > 69) && (risk_val <= 100)) { level = "HIGH"; };
+            nextCard.querySelector('[' + RESULTS_SUM + ']').innerHTML = "Based on your results, you marked " + "<b>" + risk_val + "</b>" + "% of your answers as yes, indicating a " + "<b>" + level
+                + "</b>" + " risk the person is a victim of Human Trafficking";
+            nextCard.querySelector('[' + RESULTS_DISPLAY_ATTR + ']').innerHTML = '<pre style="text-align:left;">' + JSON.stringify(answers, null, 2) + '</pre>';
+
+
+
+        }
+        nextQuestion(currentCard, nextCard);
     }
 
     this.init = function() {
